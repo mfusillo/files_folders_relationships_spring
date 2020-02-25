130bbe6f57dfc5efcbd9dbd82cf148bddec3afcd
Matteo Fusillo <matt.fusillo@gmail.com>
2020-02-25 15:51:38 +0000

    set up file structure

---
+++ src/main/java/com/codeclan/example/filesfoldersrelationships/FilesfoldersrelationshipsApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.filesfoldersrelationships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesfoldersrelationshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesfoldersrelationshipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/filesfoldersrelationships/FilesfoldersrelationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.filesfoldersrelationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesfoldersrelationshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
